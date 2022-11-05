cdfe6ae896ec75f88b5c8aa910399ff6dee63917
Deivax <>
2022-11-05 16:30:08 +0100

    Remove comment

--- src/backend/controllers/UserDataUnionJoiner.ts
+++ src/backend/controllers/UserDataUnionJoiner.ts
@@ -2,7 +2,6 @@ import DataUnionClient from "@dataunions/client";
 import { ADMIN_PRIVATE_KEY } from "../constants";
 import { User } from "../domain/User";
 
-// dataUnionAddress = "0x0D483E10612F327FC11965Fc82E90dC19b141641"
 export class UserDataUnionJoiner {
   public async join(user: User, dataUnionAddress: string): Promise<void> {
     const dataUnionClient = new DataUnionClient({
